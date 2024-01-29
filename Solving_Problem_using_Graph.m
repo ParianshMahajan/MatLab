90cc08fc01cab7cdd6b97bb1858155513dd8454e
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-01-29 10:29:48 +0530

    Update and rename Plotting_a_graph.m to Solving_Problem_using_Graph.m

--- Solving_Problem_using_Graph.m
+++ Solving_Problem_using_Graph.m
@@ -7,6 +7,9 @@ rhs=[24 ; 6 ; 1 ; 2 ; 0 ; 0]                                            %constra
 
 
 
+
+%PLOTTING THE GREAPH
+
 x1=0:2:max(rhs);                                                        %start:gap:end
 x21=(rhs(1)-cons(1,1)*x1)/cons(1,2);
 x22=(rhs(2)-cons(2,1)*x1)/cons(2,2);
@@ -22,3 +25,26 @@ plot(x1,x21,'b',x1,x22,'g',x1,x23,'r',x1,x24,'y');
 title('Plotting a graph');
 xlabel('X-Axis');
 ylabel('Y-Axis');
+
+
+
+
+
+
+%FINDING THE INTERSECTION
+
+sol=[];
+for i=1:6
+    A1=cons(i,:);
+    B1=rhs(i,:);
+    
+    for j=i+1:6
+        A2=cons(j,:);
+        B2=rhs(j,:);
+        
+        A3=[A1,A2];
+        B3=[B1,B2];
+        X=inv(A3)*B3;
+        sol=[sol X];
+    end
+end
